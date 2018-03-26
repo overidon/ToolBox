31ebbcda32fa2551a752edda7662640be10406d6
Tyler Stansfield Jaggers <32966867+overidon@users.noreply.github.com>
2018-03-26 15:44:29 -0700

    Add files via upload
    TrigTraining
    Usage of the Unit Circle ETC, xComponent, yComponent, sin, cosine etc.

---
+++ trig/colors.js
@@ -0,0 +1,19 @@
+// MARK: Setup colors
+var BLACK =        "rgb(" +   0 + "," +   0 + "," +   0 + ")";
+var BLUE =         "rgb(" +   0 + "," +  57 + "," + 230 + ")";
+var YELLOW =       "rgb(" + 230 + "," + 230 + "," +   0 + ")";
+var GREEN =        "rgb(" +   0 + "," + 230 + "," +   0 + ")";
+var RED =          "rgb(" + 250 + "," +   0 + "," +   0 + ")";
+var HEAVYORANGE =  "rgb(" + 255 + "," + 130 + "," +  10 + ")";
+var HAZE =         "rgb(" +   0 + "," +  25 + "," +  50 + ")";
+var NUKE =         "rgb(" + 255 + "," +   0 + "," + 128 + ")";
+var ARMYGREEN =    "rgb(" +  32 + "," +  96 + "," +  32 + ")";
+var DARKVIOLET =   "rgb(" +  88 + "," +  67 + "," + 189 + ")";
+var DARKNAVY =     "rgb(" +  26 + "," +   0 + "," + 200 + ")";
+var DARKPINK =     "rgb(" + 179 + "," +   0 + "," +  71 + ")";
+var MENUBLUE =     "rgb(" + 219 + "," + 248 + "," + 252 + ")";
+var MATHORANGE =   "rgb(" + 255 + "," + 230 + "," + 128 + ")";
+var PLASMAYELLOW = "rgb(" + 255 + "," + 255 + "," +  30 + ")";
+var MAROON =       "rgb(" + 128 + "," +   0 + "," +   0 + ")";
+var DARKORCHID =   "rgb(" + 153 + "," +  50 + "," + 204 + ")";
+var PURPLE     =   "rgb(" + 200 + "," +  10 + "," + 204 + ")";
