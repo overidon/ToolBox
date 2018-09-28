f301829a67e6a9c9f52e127f5189f21fa0fcf421
Tyler Stansfield Jaggers <32966867+overidon@users.noreply.github.com>
2018-09-28 14:36:03 -0700

    Create force_click.js
    Force a Javascript click on an HTML element such as a link

---
+++ JAVASCRIPT_TOOLBOX/force_click.js
@@ -0,0 +1,12 @@
+// imagine that there is an href (LINK) in your index.html or some html file...
+
+// you want to force a click on that specific link without actually making anyone click it
+
+// if the link has an id of 'link_test' ------> 
+  
+///<a id="link_test" href="https://www.overidon.com" id="msft_scan">link_test</a>
+    
+// then from an appropriate place in javascript such as within a button, or more appropriately like a 
+// function that normally isn't connected to the UI, you could summon a basic click by doing...
+
+document.getElementById('link_test').click();
