defdfaadf9b1085d0ae1a8a0121dfa16fb3bb671
Tyler Stansfield Jaggers <32966867+overidon@users.noreply.github.com>
2019-06-15 16:58:20 -0700

    Update IsPrime.java
    optimized...as of 6_15_2019

--- JAVA_TOOLBOX/IsPrime.java
+++ JAVA_TOOLBOX/IsPrime.java
@@ -16,42 +16,20 @@ public class IsPrime {
 	public static void main(String[] args) {
 
 		System.out.println(isPrime(1));
+		System.out.println(isPrime(17));
+		System.out.println(isPrime(402));
 	}
 	
 	
 	public static boolean isPrime(int input	){
 		
-		// this caveat can be contested as the definitions of Mathematics expand over time...
-		// but for now this is fine
-		if (input < 2) {
-			
-			return (false);
+		if ( n < 2 ) return false; 
+		
+		for (int i = 2; i < n; i++) {
+			if ( n  % i == 0 ) return false; 
 		}
 		
-		if (input % 2 == 0) {
-			System.out.println(" even NUMBER detected.........");
-			return (false);
-		}
-		
-		
-		for (int i = 3; i <= Math.sqrt(input); i+=2){
-			
-			if (input > i){
-				
-				// this module check will see if the number is divisible by a basic natural number...
-				// note how this check makes sure that the loop iterator of 'i' isn't larger or equal to..
-				// the input itself....
-
-				System.out.println("Input modulo i of: " + i + " is: " + ( input % i ));
-						
-				if (input % i == 0){
-					return (false);
-				}
-			}
-		}
-		
-		// if a number can make it through the sieve without returning false it is true
-		return (true);
+		return true;
 	}
 
 }
