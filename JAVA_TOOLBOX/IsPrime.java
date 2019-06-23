3001d4e405f6bde79a1d9494ab8a73d673393ec6
Tyler Stansfield Jaggers <32966867+overidon@users.noreply.github.com>
2019-06-22 22:29:08 -0700

    Update IsPrime.java

--- JAVA_TOOLBOX/IsPrime.java
+++ JAVA_TOOLBOX/IsPrime.java
@@ -18,18 +18,34 @@ public class IsPrime {
 		System.out.println(isPrime(1));
 		System.out.println(isPrime(17));
 		System.out.println(isPrime(402));
+		
+		// loop through and see if any of these numbers are primes... then output the set
+		LinkedList<Integer> primes = new LinkedList<Integer>();
+
+		for (int i = 0; i < 200; i++){
+			
+			if ( isPrime(i)){
+				System.out.println("\nIt is " + isPrime(i) + " that " + i + " is a prime number.\n");
+				primes.add(i);
+			} else {
+				System.out.println(i + " is NOT PRIME...");
+			}
+			
+		}
+		
+		System.out.println(primes);
+		
 	}
 	
 	
-	public static boolean isPrime(int input	){
+	public static boolean isPrime( int n ){
 		
-		if ( n < 2 ) return false; 
-		
-		for (int i = 2; i < n; i++) {
-			if ( n  % i == 0 ) return false; 
+		if ( n <= 1) return false; 
+				
+		for (int i = 2; i <  (int) (n / 2) + 1; i++){
+			if ( n % i == 0) return false;  
 		}
-		
-		return true;
+		return true; 
 	}
 
 }
