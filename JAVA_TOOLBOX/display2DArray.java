7b41089de726278aa4148aedb90bc1059afa198d
Tyler Stansfield Jaggers <32966867+overidon@users.noreply.github.com>
2019-07-06 23:23:47 -0700

    Update display2DArray.java

--- JAVA_TOOLBOX/display2DArray.java
+++ JAVA_TOOLBOX/display2DArray.java
@@ -1,3 +1,4 @@
+	  
 	  public static void display2DArray(int[][] arr) {
 
 		  for (int row = 0; row < arr.length; row ++ ){
@@ -5,13 +6,13 @@
 			  System.out.print("\n[ ");
 				
 				for (int i = 0; i < arr[0].length; i++) {
-					if (i == arr.length - 1) {
+					if (i == arr[0].length - 1) {
 						System.out.print(arr[row][i]);
 					} else {
 						System.out.print(arr[row][i] + ", ");
 					}
 				}
 				
-				System.out.print(" ]\n");
+				System.out.print(" ]");
 		  }	
-		}
+	  }
