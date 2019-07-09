e945aeb3d770a02af22b9942f9d69c1dc83dc511
Tyler Stansfield Jaggers <32966867+overidon@users.noreply.github.com>
2019-07-09 14:02:09 -0700

    Create prepDigitsMap.java
    take a HashMap of char keys and char[] arrays etc and then fill them with the appropriate phone digit -> letters

---
+++ JAVA_TOOLBOX/prepDigitsMap.java
@@ -0,0 +1,14 @@
+	public static void prepDigits (HashMap<Character, char[]> map  ) {
+		
+		map.put( '2', new char[]{'A', 'B', 'C'});
+		map.put( '3', new char[]{'D', 'E', 'F'});
+		map.put( '4', new char[]{'G', 'H', 'I'});
+		map.put( '5', new char[]{'J', 'K', 'L'});
+		map.put( '6', new char[]{'M', 'N', 'O'});
+		map.put( '7', new char[]{'P', 'R', 'S'});
+		map.put( '8', new char[]{'T', 'U', 'V'});
+		map.put( '9', new char[]{'W', 'X', 'Y'});
+
+		
+		
+	}
