e49a84ec8fbc5f88580a2022b8d63764d4f496f0
Tyler Stansfield Jaggers <32966867+overidon@users.noreply.github.com>
2020-01-28 12:07:45 -0800

    Update displaySubArray.java
    safeguard for IOOB on the far right.

--- JAVA_TOOLBOX/displaySubArray.java
+++ JAVA_TOOLBOX/displaySubArray.java
@@ -1,9 +1,13 @@
 	public static void displaySubArray(int [] arr, int start, int stop) {
 		
+		int L = arr.length; 
+		
+		if ( stop >= L) stop = L - 1; 
+		
 		System.out.print("\n[ ");
 		
 		for (int i = start; i <= stop; i++) {
-			if (i == arr.length - 1) {
+			if (i == L - 1) {
 				System.out.print(arr[i]);
 			} else {
 				System.out.print(arr[i] + ", ");
